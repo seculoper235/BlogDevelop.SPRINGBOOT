bc3a92e94511a9294a6f4e11bf207a50ca7c10b7
seculoper <mir8725@naver.com>
2021-07-05 23:38:46 +0900

    OAuthService 객체 수정

--- src/main/java/com/example/blogdevelop/Security/Service/OAuthService.java
+++ src/main/java/com/example/blogdevelop/Security/Service/OAuthService.java
@@ -13,9 +13,11 @@ import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
 import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
 import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
 import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.stereotype.Service;
 
 import javax.servlet.http.HttpSession;
 
+@Service
 @RequiredArgsConstructor
 public class OAuthService implements OAuth2UserService<OAuth2UserRequest, OAuth2User> {
     private final UserRepository userRepository;
