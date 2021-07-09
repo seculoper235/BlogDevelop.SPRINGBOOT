c49b19551a7ef26031271084ef767df717a5ba88
seculoper <mir8725@naver.com>
2021-07-09 23:11:13 +0900

    RegistInfo Mapper 객체 생성

---
+++ src/main/java/com/example/blogdevelop/Security/Dto/Mapper/InfoMapper.java
@@ -0,0 +1,17 @@
+package com.example.blogdevelop.Security.Dto.Mapper;
+
+import com.example.blogdevelop.Domain.User;
+import com.example.blogdevelop.Security.Dto.RegistInfo;
+
+public class InfoMapper {
+    public static User toEntity(RegistInfo registInfo) {
+        return User.builder()
+                .id(registInfo.getNickName())
+                .password("1234")
+                .username(registInfo.getUsername())
+                .email(registInfo.getEmail())
+                .description(registInfo.getDescription())
+                .build()
+                ;
+    }
+}
