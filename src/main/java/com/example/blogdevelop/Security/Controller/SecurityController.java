ca257201d7cb73a4e5fa3ffd1eec779444d464e5
seculoper <mir8725@naver.com>
2021-07-09 23:22:54 +0900

    REST API 기반의 Service 객체 작성

--- src/main/java/com/example/blogdevelop/Security/Controller/SecurityController.java
+++ src/main/java/com/example/blogdevelop/Security/Controller/SecurityController.java
@@ -4,27 +4,31 @@ import com.example.blogdevelop.Security.Dto.OAuthResponse;
 import com.example.blogdevelop.Security.Dto.RegistInfo;
 import com.example.blogdevelop.Security.Service.SecurityService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
+import org.springframework.security.oauth2.core.OAuth2AccessToken;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpSession;
+import java.io.NotSerializableException;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping(value = "/auth")
 public class SecurityController {
-    private final HttpSession httpSession;
     private final SecurityService securityService;
 
     // Social 인증 후, 애플리케이션 토큰을 발급받기 위한 API
     @PostMapping("/google")
-    public OAuthResponse authenticationGoogle(@RequestBody String accessToken) {
+    public OAuthResponse authenticationGoogle(@RequestBody String accessToken) throws NotSerializableException {
         // TODO: 토큰) OAuth의 어세스 토큰으로 유저 정보를 가져와서 스토리지에 (토큰, 유저 정보)로 저장하고, 어세스 토큰을 반납한다
         //       세션) Spring 자체의 OAuth를 이용하면 된다. 따라서 작성 필요 X
         // redirect-url으로 프론트에게 access token을 보내고, 프론트는 access token을 서버로 보낸다.
         // 서버는 받은 access token을 가지고, Social scope를 여기서 Session 객체에 저장한다(userinfo.profile에 요청을 보내서 유저 정보를 얻어온다)
         // 굳이 클라이언트에서 access token을 받아오는 이유?
         // -> 서버가 모두 처리할 수 있지만, 그렇게 되면 REST API의 의미가 사라지며 올인원 애플리케이션 구조를 띄게 된다. 따라서 REST API를 사용하려면, access token을 따로 받아야 한다
-        return new OAuthResponse();
+        return securityService.googleAuthentication(accessToken);
     }
 
     // 추가 사용자 정보를 등록하기 위한 API
--- src/main/java/com/example/blogdevelop/Security/Service/SecurityService.java
+++ src/main/java/com/example/blogdevelop/Security/Service/SecurityService.java
@@ -6,18 +6,33 @@ import com.example.blogdevelop.Security.Dto.Mapper.InfoMapper;
 import com.example.blogdevelop.Security.Dto.OAuthResponse;
 import com.example.blogdevelop.Security.Dto.RegistInfo;
 import com.example.blogdevelop.Security.Dto.SessionUser;
+import com.example.blogdevelop.Security.Dto.UserDto;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import javax.servlet.http.HttpSession;
+import java.io.NotSerializableException;
 import java.util.NoSuchElementException;
 
 @Service
 @RequiredArgsConstructor
+@PropertySource({"classpath:application-oauth.properties"})
 public class SecurityService {
     // 의존성 주입
+    @Value("${spring.security.oauth2.client.provider.google.user-info-uri}")
+    private String GOOGLE_USERINFO;
     private final UserRepository userRepository;
     private final HttpSession httpSession;
+    private final ObjectMapper objectMapper;
 
     public OAuthResponse registerUserInfo(RegistInfo registInfo) {
         // TODO: User로 toEntity 후 저장
@@ -36,16 +51,42 @@ public class SecurityService {
 
     // redirect-url으로 프론트에게 access token을 보내고, 프론트는 access token을 서버로 보낸다.
     // TODO: 서버는 받은 access token을 가지고, Social scope를 여기서 Session 객체에 저장한다(userinfo.profile에 요청을 보내서 유저 정보를 얻어온다)
-    public OAuthResponse googleAuthetication(String accessToken) {
+    public OAuthResponse googleAuthentication(String accessToken) throws NotSerializableException {
         // 유저 정보를 가져옴
+        UserDto userDto = userInfo(accessToken);
 
         // 가져온 정보를 세션 객체에 담아서 세션에 저장
+        httpSession.setAttribute("googleUser", new SessionUser.Google(userDto));
 
         // OAuthResponse에 요소를 채워서 반환(isRegistered=false)
-        return new OAuthResponse();
+        return OAuthResponse.builder()
+                .isRegistered(false)
+                .oAuthToken(accessToken)
+                .build();
     }
 
-    public void userInfo(String accessToken) {
+    public UserDto userInfo(String accessToken) throws NotSerializableException {
         // 유저 정보를 가져옴
+        RestTemplate restTemplate = new RestTemplate();
+        HttpHeaders headers = new HttpHeaders();
+
+        headers.set("Authorization", "Bearer" + accessToken);
+        HttpEntity<String> httpEntity = new HttpEntity<>(headers);
+
+        String tokenBody = restTemplate.exchange(GOOGLE_USERINFO, HttpMethod.POST, httpEntity, String.class).getBody();
+        System.out.println("TokenBody = " + tokenBody);
+
+        JsonNode node;
+        try {
+            node = objectMapper.readTree(tokenBody);
+
+            return UserDto.builder()
+                    .username(node.get("picture").asText())
+                    .email(node.get("email").asText())
+                    .image(node.get("picture").asText())
+                    .build();
+        } catch (JsonProcessingException e) {
+            throw new NotSerializableException();
+        }
     }
 }
