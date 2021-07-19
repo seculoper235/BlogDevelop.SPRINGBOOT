90c51284bdef10a89f8569087f96a45729e2b95f
seculoper <mir8725@naver.com>
2021-07-19 20:37:32 +0900

    JwtFilter 및 Provider 작성

--- src/main/java/com/example/blogdevelop/Security/Config/JwtFilter.java
+++ src/main/java/com/example/blogdevelop/Security/Config/JwtFilter.java
@@ -1,21 +1,34 @@
 package com.example.blogdevelop.Security.Config;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.GenericFilterBean;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
 
+@Slf4j
 @RequiredArgsConstructor
 public class JwtFilter extends GenericFilterBean {
     private final JwtProvider jwtProvider;
 
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
-        // TODO: HwtProvider로 JWT 검증 후, Authentication을 생성하여 SecurityContext에 저장
+        // JwtProvider로 JWT 검증 후, Authentication을 생성하여 SecurityContext에 저장
+        String jwt = jwtProvider.resolveJwt((HttpServletRequest) servletRequest);
+
+        if(jwt != null && jwtProvider.validateJwt(jwt)) {
+            Authentication authentication = jwtProvider.getAuthentication(jwt);
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        }
+        else
+            log.error("JWT가 존재하지 않거나, 올바르지 않은 JWT 입니다.");
 
         filterChain.doFilter(servletRequest, servletResponse);
     }
--- src/main/java/com/example/blogdevelop/Security/Config/JwtProvider.java
+++ src/main/java/com/example/blogdevelop/Security/Config/JwtProvider.java
@@ -1,15 +1,24 @@
 package com.example.blogdevelop.Security.Config;
 
 import com.example.blogdevelop.Security.Service.JwtService;
-import io.jsonwebtoken.io.Decoder;
+import io.jsonwebtoken.*;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import io.jsonwebtoken.security.SignatureException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
+import javax.servlet.http.HttpServletRequest;
 import java.security.Key;
+import java.util.Date;
+import java.util.List;
 
+@Slf4j
 @Component
 @PropertySource({"classpath:application-jwt.properties"})
 public class JwtProvider {
@@ -18,9 +27,60 @@ public class JwtProvider {
     private final long tokenValidTime = 30 * 60 * 1000L;
     private final JwtService jwtService;
 
-    public JwtProvider(@Value("jwt.secret") String secret, JwtService jwtService) {
+    public JwtProvider(@Value("${jwt.secret}") String secret, JwtService jwtService) {
         this.secret = secret;
         this.secretKey = Keys.hmacShaKeyFor(Decoders.BASE64.decode(secret));
         this.jwtService = jwtService;
+    }
+
+    public String createJwt(String username, List<String> authorities) {
+        Date date = new Date();
+
+        return Jwts.builder()
+                .setSubject(username)
+                .claim("auth", authorities)
+                .setIssuedAt(date)
+                .setExpiration(new Date(date.getTime() + tokenValidTime))
+                .signWith(secretKey, SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    public String resolveJwt(HttpServletRequest request) {
+        return request.getHeader("X-AUTH-TOKEN");
+    }
+
+    public Authentication getAuthentication(String jwt) {
+        String username = Jwts.parserBuilder()
+                .setSigningKey(secretKey)
+                .build()
+                .parseClaimsJws(jwt)
+                .getBody().getSubject();
+
+        UserDetails user = jwtService.loadUserByUsername(username);
+
+        return new UsernamePasswordAuthenticationToken(user, jwt, user.getAuthorities());
+    }
+
+    public boolean validateJwt(String jwt) {
+        try {
+            Jwts.parserBuilder()
+                    .setSigningKey(secretKey)
+                    .build()
+                    .parseClaimsJws(jwt)
+                    .getBody();
+
+            return true;
+        } catch (ExpiredJwtException e) {
+            log.error("유효하지 않은 JWT 입니다.", e);
+        } catch (UnsupportedJwtException e) {
+            log.error("지원하지 않는 JWT 입니다.", e);
+        } catch (MalformedJwtException e) {
+            log.error("JWT의 구조가 올바르지 않습니다.", e);
+        } catch (SignatureException e) {
+            log.error("올바르지 않은 JWT 서명 입니다.", e);
+        } catch (IllegalArgumentException e) {
+            log.error("잘못된 JWT 입니다.", e);
+        }
+        return false;
     }
 }
