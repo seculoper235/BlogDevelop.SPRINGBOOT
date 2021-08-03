d82dffad63d4a02892694fa8b861356460b698af
seculoper <mir8725@naver.com>
2021-08-03 23:42:11 +0900

    test: 폴더 생성 메소드 코드 테스트

--- src/test/java/com/example/blogdevelop/Service/FileServiceTest.java
+++ src/test/java/com/example/blogdevelop/Service/FileServiceTest.java
@@ -1,19 +1,21 @@
 package com.example.blogdevelop.Service;
 
+import com.example.blogdevelop.Domain.User;
 import com.example.blogdevelop.Repository.FileRepository;
 import com.example.blogdevelop.Repository.PostRepository;
 import com.example.blogdevelop.Repository.UserRepository;
+import com.example.blogdevelop.Util.FileDto;
 import com.example.blogdevelop.Web.Setting.Dto.ImageType;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
 
-import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 
@@ -30,29 +32,83 @@ public class FileServiceTest {
 
     private String absolutePath = "/tmp/uploads/";
 
-    @Test
-    @DisplayName("프로필 폴더 생성 테스트")
-    public void create_Profile_Directory_Test() {
-        // 프로필 폴더 생성 파라미터(유저 Id, 이미지 타입)
-        String userId = "user1";
-        String catId = "";
-        String postId = "";
+    String userId = "user1";
 
+    @Before
+    @DisplayName("유저 폴더 미리 생성")
+    public void init() {
+        new File("/tmp/uploads/user1").mkdir();
+    }
+
+    @Test
+    @DisplayName("기초 폴더(유저, 소개, 프로필 파일 저장 폴더) 생성 테스트")
+    public void create_Init_Directory_Test() {
         // 기대 경로
-        String expectedPath = "/tmp/uploads/user1/profiles";
+        String expectedProfilePath = "/tmp/uploads/user1/profiles";
 
         // 결과 경로
-        String resultPath = "/profiles";
+        String resultProfilePath = "/profiles";
+
+        // 폴더 생성 코드 실행
+        if (!new File(absolutePath, userId + resultProfilePath).exists()) {
+            System.out.println("프로필 폴더 생성");
+            if(!new File(absolutePath, userId + resultProfilePath).mkdir()) {
+                System.out.println("프로필 폴더 생성 실패!");
+            }
+        }
+
+        assertTrue("프로필 폴더 생성 성공!", new File(expectedProfilePath).exists());
+    }
+
+    @Test
+    @DisplayName("카테고리 폴더 생성 테스트")
+    public void create_Cat_Directory_Test() {
+        // 포스트 폴더 생성 파라미터(유저 Id, 카테고리 Id)
+        String catId = "cat_ID";
+
+        // 기대 경로
+        String expectedPath = "/tmp/uploads/user1/posts/cat_ID";
+
+        // 결과 경로
+        String resultPath = "/posts/" +catId;
+
+        // 폴더 생성 코드 실행
         File resultFIle = new File(absolutePath, userId + resultPath);
         if (!new File(absolutePath, userId + resultPath).exists()) {
-            System.out.println("폴더 생성");
+            System.out.println("카테고리 폴더 생성");
             if(!new File(absolutePath, userId + resultPath).mkdir()) {
-                System.out.println("생성 실패!");
+                System.out.println("카테고리 폴더 생성 실패!");
             }
         }
 
         assertTrue(new File(expectedPath).exists());
     }
+
+    @Test
+    @DisplayName("포스트 폴더 생성 테스트")
+    public void create_Post_Directory_Test() {
+        // 포스트 폴더 생성 파라미터(유저 Id, 카테고리 Id, 포스트 Id)
+        String catId = "cat_ID";
+        String postId = "post_ID";
+
+        // 기대 경로
+        String expectedPath = "/tmp/uploads/user1/posts/cat_ID/post_ID";
+
+        // 결과 경로
+        String resultPath = "/posts/" +catId+ "/" +postId;
+
+        // 폴더 생성 코드 실행
+        File resultFIle = new File(absolutePath, userId + resultPath);
+        if (!new File(absolutePath, userId + resultPath).exists()) {
+            System.out.println("포스트 폴더 생성");
+            if(!new File(absolutePath, userId + resultPath).mkdir()) {
+                System.out.println("포스트 폴더 생성 실패!");
+            }
+        }
+
+        assertTrue(new File(expectedPath).exists());
+    }
+
 
     @Test
     @DisplayName("프로필 업로드 테스트")
