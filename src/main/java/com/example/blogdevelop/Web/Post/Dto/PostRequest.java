4bf73881c1699456a63b6a1502dabfed0b88dd76
seculoper <mir8725@naver.com>
2021-08-07 22:37:23 +0900

    update: PostRequest 작성

--- src/main/java/com/example/blogdevelop/Web/Post/Dto/PostRequest.java
+++ src/main/java/com/example/blogdevelop/Web/Post/Dto/PostRequest.java
@@ -1,4 +1,20 @@
 package com.example.blogdevelop.Web.Post.Dto;
 
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
 public class PostRequest {
+    private int postId;
+    private String title;
+    private String content;
+
+    @Builder
+    public PostRequest(int postId, String title, String content) {
+        this.postId = postId;
+        this.title = title;
+        this.content = content;
+    }
 }
