40d87eec4daf30f47a0f50d5383b0414ca9b1d0a
seculoper <mir8725@naver.com>
2021-08-21 23:05:51 +0900

    update: 조회 메소드 추가

--- src/main/java/com/example/blogdevelop/Repository/PostRepository.java
+++ src/main/java/com/example/blogdevelop/Repository/PostRepository.java
@@ -3,5 +3,8 @@ package com.example.blogdevelop.Repository;
 import com.example.blogdevelop.Domain.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface PostRepository extends JpaRepository<Post, Integer> {
+    List<Post> findAllByCategory_Id(int cat);
 }
